8d2de845473351cab74e39d26dc0abb654ff276c
hp <tsaroj896@gmail.com>
2021-02-25 10:10:49 +0545

    area

--- ace/zoro.py
+++ ace/zoro.py
@@ -1,7 +1,7 @@
 #wrtie a program that reads the leangth of base and height of the right angle triangle and print the area.
 #every number is given in a seperate line.
 base= int(input("enter the lenght of base:"))
-height= int(input("enter the legth of height"))
+height= int(input("enter the length of height"))
 
-area=1/2*base*height
+area=(base * height)//2
 print(f"the area of the triangle is{area}")
