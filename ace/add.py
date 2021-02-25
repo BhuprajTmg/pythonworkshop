330c1f5643241efed1fe41954c0b961366eda47d
hp <tsaroj896@gmail.com>
2021-02-25 09:26:08 +0545

    Initial commit

---
+++ ace/add.py
@@ -0,0 +1,9 @@
+#write a program that takes three numbers and prints their sum.
+# Every number is given on a separate line.
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+num3=int(input("enter third number:"))
+
+sum=num1 + num2 +num3
+
+print("sum of the numbers is :",sum)
